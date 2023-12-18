13cd948a2ab7492ba0a9e1cdef49a9f2e766b9a4
Francisco Laviano <linespi39@gmail.com>
2023-12-18 14:10:36 -0300

    primerEntregaJava

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+package src;
+public class Main {
+
+    public static void main(String[] args) {
+
+        String primerNombre = "Francisco";
+        String segundoNombre = "Antonio";
+        String apellido = "Laviano";
+        String segundoApellido = "Colman";
+
+        int edad = 42;
+
+        System.out.println(
+          "Nombre completo: "+primerNombre +" "+ segundoNombre +" "+ apellido +" "+ segundoApellido +"; "+
+          "Edad: " + edad
+          
+
+        );
+        
+    }
+
+    
+}
