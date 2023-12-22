0aadade99bab6d77b40c282faac3ecc943fca32f
Francisco Laviano <linespi39@gmail.com>
2023-12-22 20:52:33 -0300

    tercera re-entrega 1

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         
-     System.out.println("PRESTAMOS A SOLA FIRMA EN U$S");
+     float number = 22.176f;
+     int unit = 1;
 
-       // La moneda de monto, cuota e interesesTotales es U$S
-        float monto = 500.16f;
-      //cantidadCuotas es la cantidad de meses que voy a demorar en pagar el préstamo
-        int cantidadCuotas = 29;
+     float modulo = number % unit;
 
-       System.out.println("El monto solicitado es: "+ monto + " U$S");
+     int parteEntera = (int)number / unit;
 
-        float intereses  ;
-        if(cantidadCuotas<=6) {intereses = 1.00f;} else if(cantidadCuotas>6 && cantidadCuotas<24) {intereses = 1.13f;} else {intereses=1.28f;}
-        
-      //cuota es lo que tengo que pagar este mes si no pagué todo el préstamo
-       float cuota = (monto / cantidadCuotas)  ;
-
-
-        float interesTotal =  cuota * intereses * cantidadCuotas;
-
-        System.out.println("La cuota de este mes es de: "+ cuota + " U$S");
-
-        System.out.println("El pago total es de: "+interesTotal+" U$S");
+     System.out.println(number);
+     System.out.println("El modulo es: "+modulo);
+     System.out.println("Su parte entera es: "+parteEntera);
     }
     
 }
