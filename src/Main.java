5b23b5765b6606084684d55c02e0101ff5aa7860
Francisco Laviano <linespi39@gmail.com>
2023-12-22 02:12:06 -0300

    primer entrega java con formulas mejoradas

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,16 @@ public class Main {
       //cantidadCuotas es la cantidad de meses que voy a demorar en pagar el préstamo
         int cantidadCuotas = 8;
 
-       
+       System.out.println("El monto solicitado es: "+ monto + " U$S");
 
-        float intereses = 0.13f ;
+        float intereses  ;
+        if(cantidadCuotas<=6) {intereses = 1.00f;} else if(cantidadCuotas>6 && cantidadCuotas<24) {intereses = 1.13f;} else {intereses=1.28f;}
         
       //cuota es lo que tengo que pagar este mes si no pagué todo el préstamo
-       float cuota = (monto / cantidadCuotas) * (1.00f+0.13f) ;
+       float cuota = (monto / cantidadCuotas)  ;
 
 
-        float interesTotal =  monto * intereses * cantidadCuotas;
+        float interesTotal =  cuota * intereses * cantidadCuotas;
 
         System.out.println("La cuota de este mes es de: "+ cuota + " U$S");
 
