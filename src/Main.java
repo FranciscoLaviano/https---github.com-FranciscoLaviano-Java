4847dfb32346c9a31ce9ff825375e00d6b34d787
Francisco Laviano <linespi39@gmail.com>
2023-12-21 23:43:52 -0300

    re-entrega1

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ public class Main {
         
      System.out.println("PRESTAMOS A SOLA FIRMA EN U$S");
 
-       // La moneda es U$S
+       // La moneda de monto, cuota e interesesTotales es U$S
         float monto = 500.16f;
-
+      //cantidadCuotas es la cantidad de meses que voy a demorar en pagar el préstamo
         int cantidadCuotas = 8;
 
+       
+
         float intereses = 0.13f ;
-
-
+        
+      //cuota es lo que tengo que pagar este mes si no pagué todo el préstamo
        float cuota = (monto / cantidadCuotas) * (1.00f+0.13f) ;
 
 
